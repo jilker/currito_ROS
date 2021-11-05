7dff3ea3ad603a1cc4b32773e2da928e5cf91275
Victor Quesada Conejero <victorqc09@gmail.com>
2021-11-05 19:18:59 +0100

    Add: servo dumb script

---
+++ scripts/servos.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import UInt16
+
+class Servo():
+    def __init__(self):
+        self.nh = rospy.init_node('pc_servo')
+        self.pub = rospy.Publisher("/servo",UInt16,queue_size=10)
+    def move(self,value):
+        msg = UInt16()
+        msg.data = value
+        self.pub.publish(msg)
+if __name__ == "__main__":
+    servo = Servo()
+    rate = rospy.Rate(1)
+    while not rospy.is_shutdown():
+        value = input("value: ")
+        servo.move(value)
+        rate.sleep()
+
+    
