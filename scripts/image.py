5579c5f2c929ef18e37438ac571f0a2206a53f16
Manuel Batista <manbtsn@gmail.com>
2021-12-09 16:47:19 +0100

    Added HoughCircles to detection and tool to calibrate HSV

--- scripts/image.py
+++ scripts/image.py
@@ -10,7 +10,7 @@ import cv2
 import rospy
 
 from sensor_msgs.msg import Image
-from sensor_msgs.msg import CameraInfo
+# from sensor_msgs.msg import CameraInfo
 import numpy as np
 
 class CameraCurrito():
@@ -18,33 +18,36 @@ class CameraCurrito():
         self.node = rospy.init_node("image_currito", anonymous=True)
         self.img_pub = rospy.Publisher("/camera/image_raw", Image,queue_size=10)
         self.video = cv2.VideoCapture(0)
-        self.height = 0
-        self.width = 0
+        # self.height = 0
+        # self.width = 0
     def loop(self):
         while not rospy.is_shutdown() and self.video.grab():
-            tmp,img = self.get_image()
-            self.height,self.width = img.shape[0:2]
+            img = self.get_image()
+
+            # self.height,self.width = img.shape[0:2]
+
             mask = self.get_mask(img)
-            # res = cv2.bitwise_and(img,img,mask=mask)
-            ret,res = cv2.threshold(img[:,:,1],100,255,cv2.THRESH_BINARY)
-            contours  = cv2.findContours(mask, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-            if contours[1].__len__() >0:
-                for it in contours[1]:
-                    cv2.drawContours(img, it, -1, (0,255,0), 3)
-                c = max(contours[1], key=cv2.contourArea)
-                # ((x, y), radius) = cv2.minEnclosingCircle(c)
-                M = cv2.moments(c)
-                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-                cv2.circle(img, center, 5, (0, 0, 255), -1)
-            cv2.imshow('mask',mask)
-            cv2.imshow('res',res)
-            cv2.imshow("frame",img)
-            # cv2.imshow("cnts", cnts)
+            res = cv2.bitwise_and(img,img,mask=mask)
+            res = cv2.cvtColor(res,cv2.COLOR_BGR2GRAY)
+
+            circles = cv2.HoughCircles(res,cv2.HOUGH_GRADIENT,1,600,param1=50,param2=12,minRadius=10,maxRadius=500)
+            try:
+                circles = np.uint16(np.around(circles))
+                
+                for circle in circles[0,:]:
+                    # print(circle)
+                    # draw the outer circle
+                    cv2.circle(img,(circle[0],circle[1]),circle[2],(0,255,0),2)
+                    # draw the center of the circle
+                    cv2.circle(img,(circle[0],circle[1]),2,(0,0,255),3)
+            except:
+                pass
+            cv2.imshow('detected circles',img)
             cv2.waitKey(1)
     def get_mask(self,img):
         img = cv2.GaussianBlur(img, (11, 11), 0)
         hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-        color = 1
+        color = 0
         if color == 1:
             lower_red_1 = np.array([160,100,20])
             upper_red_1 = np.array([175,255,255])
@@ -58,8 +61,8 @@ class CameraCurrito():
             mask_B = cv2.dilate(mask_B, None, iterations=2)
             return mask_A+mask_B
         else:
-            lower_red_1 = np.array([18,50,100])
-            upper_red_1 = np.array([35,255,255])
+            lower_red_1 = np.array([125,15,203])
+            upper_red_1 = np.array([179,255,255])
             mask_A = cv2.inRange(hsv,lower_red_1,upper_red_1)
             mask_A = cv2.erode(mask_A, None, iterations=2)
             mask_A = cv2.dilate(mask_A, None, iterations=2)
@@ -71,7 +74,7 @@ class CameraCurrito():
             print("Could not grab frame.")
             return 0
         else:
-            return tmp,img
+            return img
 
 if __name__ == '__main__':
     try:
---
+++ scripts/sliders_hsv.py
@@ -0,0 +1,84 @@
+import cv2
+import numpy as np
+
+def callback(x):
+    pass
+
+cap = cv2.VideoCapture(0)
+cv2.namedWindow('image')
+
+ilowH = 160
+ihighH = 175
+
+ilowS = 100
+ihighS = 255
+ilowV = 20
+ihighV = 255
+
+# create trackbars for color change
+cv2.createTrackbar('lowH','image',ilowH,179,callback)
+cv2.createTrackbar('highH','image',ihighH,179,callback)
+
+cv2.createTrackbar('lowS','image',ilowS,255,callback)
+cv2.createTrackbar('highS','image',ihighS,255,callback)
+
+cv2.createTrackbar('lowV','image',ilowV,255,callback)
+cv2.createTrackbar('highV','image',ihighV,255,callback)
+
+elowH = 0
+ehighH = 10
+elowS = 100
+ehighS = 255
+elowV = 10
+ehighV = 255
+
+# create trackbars for color change
+cv2.createTrackbar('elowH','image',elowH,179,callback)
+cv2.createTrackbar('ehighH','image',ehighH,179,callback)
+
+cv2.createTrackbar('elowS','image',elowS,255,callback)
+cv2.createTrackbar('ehighS','image',ehighS,255,callback)
+
+cv2.createTrackbar('elowV','image',elowV,255,callback)
+cv2.createTrackbar('ehighV','image',ehighV,255,callback)
+
+while True:
+    # grab the frame
+    ret, frame = cap.read()
+
+    # get trackbar positions
+    ilowH = cv2.getTrackbarPos('lowH', 'image')
+    ihighH = cv2.getTrackbarPos('highH', 'image')
+    ilowS = cv2.getTrackbarPos('lowS', 'image')
+    ihighS = cv2.getTrackbarPos('highS', 'image')
+    ilowV = cv2.getTrackbarPos('lowV', 'image')
+    ihighV = cv2.getTrackbarPos('highV', 'image')
+
+    elowH = cv2.getTrackbarPos('elowH', 'image')
+    ehighH = cv2.getTrackbarPos('ehighH', 'image')
+    elowS = cv2.getTrackbarPos('elowS', 'image')
+    ehighS = cv2.getTrackbarPos('ehighS', 'image')
+    elowV = cv2.getTrackbarPos('elowV', 'image')
+    ehighV = cv2.getTrackbarPos('ehighV', 'image')
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_hsv = np.array([ilowH, ilowS, ilowV])
+    higher_hsv = np.array([ihighH, ihighS, ihighV])
+    lower_hsv2 = np.array([elowH, elowS, elowV])
+    higher_hsv2 = np.array([ehighH, ehighS, ehighV])
+    mask = cv2.inRange(hsv, lower_hsv, higher_hsv)
+    mask2 = cv2.inRange(hsv, lower_hsv2, higher_hsv2)
+
+    mask = mask + mask2
+
+    frame = cv2.bitwise_and(frame, frame, mask=mask)
+
+    # show thresholded image
+    cv2.imshow('image', frame)
+    k = cv2.waitKey(10) & 0xFF # large wait time to remove freezing
+    if k == 113 or k == 27:
+        break
+
+
+cv2.destroyAllWindows()
+cap.release()
